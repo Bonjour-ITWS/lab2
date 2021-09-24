d248cb0e79d8c848236d6ab223726150827f755c
Martha Kuitula <59888538+mmk48309@users.noreply.github.com>
2021-09-24 00:27:05 -0400

    Added Buttons 6-10
    I added the buttons for the 6th-10th amendments. I needed to add the JavaScript for each button to switch the text, the CSS for the button styling, and the HTML to have the button on the web page.

---
+++ resources/main.js
@@ -0,0 +1,53 @@
+function myFunction() {
+    var x = document.getElementById("firstAmendment");
+    if (x.innerHTML == "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.") {
+        x.innerHTML = "Freedom of speech, press, right to assemble, petition.";
+    } else {
+        x.innerHTML = "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.";
+    }
+}
+
+function mySixthAmend() {
+    var x = document.getElementById("sixthAmendment");
+    if (x.innerHTML == "In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.") {
+        x.innerHTML = "Right to a speedy trial.";
+    } else {
+        x.innerHTML = "In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.";
+    }
+}
+
+function mySeventhAmend() {
+    var x = document.getElementById("seventhAmendment");
+    if (x.innerHTML == "In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.    ") {
+        x.innerHTML = "Cannot retry a case after it's conclusion.";
+    } else {
+        x.innerHTML = "In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.    ";
+    }
+}
+
+function myEighthAmend() {
+    var x = document.getElementById("eighthAmendment");
+    if (x.innerHTML == "Excessive bail shall not be required, nor excessive fines imposed, nor cruel and unusual punishments inflicted.    ") {
+        x.innerHTML = "No cruel and unusual punishments.";
+    } else {
+        x.innerHTML = "Excessive bail shall not be required, nor excessive fines imposed, nor cruel and unusual punishments inflicted.    ";
+    }
+}
+
+function myNinthAmend() {
+    var x = document.getElementById("ninthAmendment");
+    if (x.innerHTML == "The enumeration in the Constitution, of certain rights, shall not be construed to deny or disparage others retained by the people.    ") {
+        x.innerHTML = "Constitution will not be manipulated disparage others.";
+    } else {
+        x.innerHTML = "The enumeration in the Constitution, of certain rights, shall not be construed to deny or disparage others retained by the people.    ";
+    }
+}
+
+function myTenthAmend() {
+    var x = document.getElementById("tenthAmendment");
+    if (x.innerHTML == "The powers not delegated to the United States by the Constitution, nor prohibited by it to the States, are reserved to the States respectively, or to the people.") {
+        x.innerHTML = "Remaining powers not used by Congress are re-delegated to individual states.";
+    } else {
+        x.innerHTML = "The powers not delegated to the United States by the Constitution, nor prohibited by it to the States, are reserved to the States respectively, or to the people.";
+    }
+}
