5409757a7f4ba3d7d4b6fcc1bc714390d38f9117
Tyler Haynes <72038702+haynet3@users.noreply.github.com>
2021-09-24 01:09:58 -0400

    Add files via upload

--- resources/main.js
+++ resources/main.js
@@ -1,35 +1,3 @@
-//function dosomething(){
- //   document.getElementById('firstAmendment').innerHTML = 'Freedom of speech, press, right to assemble, petition.';
- //   }
-
-//<script src="script.js" type="/resources/main.js"></script>
-
-function myArticles12(){
-
-    var x = document.getElementById("Articles12");
-    if (x.innerHTML == "Summary of Articles 1 & 2") {
-        x.innerHTML = "The articles of confederation is a way to display solidarity among soverign states. This loose union, a confederacy, will allow states to maintain their own rights, privledges and powers while on the condition that they remain part of the confederacy. The Articles of Confederation places a significant amount of power in the hands of a central government while maintaining soverign state powers. Hwoever, the Articles of Confederation remain open to interpretation today-- some say that the Articles of Confederation was a loose union between 13 soverign states in the allegiance of self-defense from Britain. That union, according to some, only exists for the sole purpose of an outside attack-- not for the entire globalization of a single country. The debate between state and federal power remains a disucssion today that stems from these very Articles of Confederation."
-        ;
-    } else {
-        x.innerHTML = "Summary of Articles 1 & 2";
-    }
-
-
-}
-
-function myArticles34(){
-
-    var x = document.getElementById("Articles34");
-    if (x.innerHTML == "Summary of Articles 3 & 4") {
-        x.innerHTML = "States must seek to help each other. By coercing all members of the union to help one another when they need it, the union ensures its success through divisive times. These articles emphasize that a citizen of one state is a citizen of another state. They also point to the roots of the American free market. By ensuring trade and seamless interpersonal relations with other soverign states, the efficient market begins to develop into the US economy")
-        ;
-    } else {
-        x.innerHTML = "Summary of Articles 3 & 4";
-    }
-
-
-}
-
 
 function myFunction() {
     var x = document.getElementById("firstAmendment");
@@ -74,47 +42,4 @@ function myFifthAmend() {
     } else {
         x.innerHTML = "No person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury, except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger; nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any criminal case to be a witness against himself, nor be deprived of life, liberty, or property, without due process of law; nor shall private property be taken for public use, without just compensation. ";
     }
-  
-function mySixthAmend() {
-    var x = document.getElementById("sixthAmendment");
-    if (x.innerHTML == "In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.") {
-        x.innerHTML = "Right to a speedy trial.";
-    } else {
-        x.innerHTML = "In all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.";
-    }
 }
-
-function mySeventhAmend() {
-    var x = document.getElementById("seventhAmendment");
-    if (x.innerHTML == "In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.    ") {
-        x.innerHTML = "Cannot retry a case after it's conclusion.";
-    } else {
-        x.innerHTML = "In Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.    ";
-    }
-}
-
-function myEighthAmend() {
-    var x = document.getElementById("eighthAmendment");
-    if (x.innerHTML == "Excessive bail shall not be required, nor excessive fines imposed, nor cruel and unusual punishments inflicted.    ") {
-        x.innerHTML = "No cruel and unusual punishments.";
-    } else {
-        x.innerHTML = "Excessive bail shall not be required, nor excessive fines imposed, nor cruel and unusual punishments inflicted.    ";
-    }
-}
-
-function myNinthAmend() {
-    var x = document.getElementById("ninthAmendment");
-    if (x.innerHTML == "The enumeration in the Constitution, of certain rights, shall not be construed to deny or disparage others retained by the people.    ") {
-        x.innerHTML = "Constitution will not be manipulated disparage others.";
-    } else {
-        x.innerHTML = "The enumeration in the Constitution, of certain rights, shall not be construed to deny or disparage others retained by the people.    ";
-    }
-}
-
-function myTenthAmend() {
-    var x = document.getElementById("tenthAmendment");
-    if (x.innerHTML == "The powers not delegated to the United States by the Constitution, nor prohibited by it to the States, are reserved to the States respectively, or to the people.") {
-        x.innerHTML = "Remaining powers not used by Congress are re-delegated to individual states.";
-    } else {
-        x.innerHTML = "The powers not delegated to the United States by the Constitution, nor prohibited by it to the States, are reserved to the States respectively, or to the people.";
-    }
