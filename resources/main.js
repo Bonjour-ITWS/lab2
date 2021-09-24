da7d80f0694b8cee9345af7466362b8729e4a694
Divya <divsbachina@gmail.com>
2021-09-24 00:37:42 -0400

    Added Descriptions for Articles 1-4
    added descriptions for Articles 1-4 with Button

---
+++ resources/main.js
@@ -0,0 +1,78 @@
+//function dosomething(){
+ //   document.getElementById('firstAmendment').innerHTML = 'Freedom of speech, press, right to assemble, petition.';
+ //   }
+
+//<script src="script.js" type="/resources/main.js"></script>
+
+
+function myArticles12(){
+
+    var x = document.getElementById("Articles12");
+    if (x.innerHTML == "Summary of Articles 1 & 2") {
+        x.innerHTML = "The articles of confederation is a way to display solidarity among soverign states. This loose union, a confederacy, will allow states to maintain their own rights, privledges and powers while on the condition that they remain part of the confederacy. The Articles of Confederation places a significant amount of power in the hands of a central government while maintaining soverign state powers. Hwoever, the Articles of Confederation remain open to interpretation today-- some say that the Articles of Confederation was a loose union between 13 soverign states in the allegiance of self-defense from Britain. That union, according to some, only exists for the sole purpose of an outside attack-- not for the entire globalization of a single country. The debate between state and federal power remains a disucssion today that stems from these very Articles of Confederation."
+        ;
+    } else {
+        x.innerHTML = "Summary of Articles 1 & 2";
+    }
+
+
+}
+
+function myArticles34(){
+
+    var x = document.getElementById("Articles34");
+    if (x.innerHTML == "Summary of Articles 3 & 4") {
+        x.innerHTML = "States must seek to help each other. By coercing all members of the union to help one another when they need it, the union ensures its success through divisive times. These articles emphasize that a citizen of one state is a citizen of another state. They also point to the roots of the American free market. By ensuring trade and seamless interpersonal relations with other soverign states, the efficient market begins to develop into the US economy")
+        ;
+    } else {
+        x.innerHTML = "Summary of Articles 3 & 4";
+    }
+
+
+}
+
+
+function myFunction() {
+    var x = document.getElementById("firstAmendment");
+    if (x.innerHTML == "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.") {
+        x.innerHTML = "Freedom of speech, press, right to assemble, petition.";
+    } else {
+        x.innerHTML = "Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.";
+    }
+}
+
+function mySecAmend() {
+    var x = document.getElementById("secAmendment");
+    if (x.innerHTML == "A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms shall not be infringed. ") {
+        x.innerHTML = "Right to bear arms.";
+    } else {
+        x.innerHTML = "A well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms shall not be infringed. ";
+    }
+}
+
+function myThirdAmend() {
+    var x = document.getElementById("thirdAmendment");
+    if (x.innerHTML == "No Soldier shall, in time of peace be quartered in any house, without the consent of the Owner, nor in time of war, but in a manner to be prescribed by law. ") {
+        x.innerHTML = "Soldier housing will only happen during times of war.";
+    } else {
+        x.innerHTML = "No Soldier shall, in time of peace be quartered in any house, without the consent of the Owner, nor in time of war, but in a manner to be prescribed by law. ";
+    }
+}
+
+function myFourthAmend() {
+    var x = document.getElementById("fourthAmendment");
+    if (x.innerHTML == "The right of the people to be secure in their persons, houses, papers, and effects, against unreasonable searches and seizures, shall not be violated, and no Warrants shall issue, but upon probable cause, supported by Oath or affirmation, and particularly describing the place to be searched, and the persons or things to be seized. ") {
+        x.innerHTML = "Search warrants are required to search personal property. ";
+    } else {
+        x.innerHTML = "The right of the people to be secure in their persons, houses, papers, and effects, against unreasonable searches and seizures, shall not be violated, and no Warrants shall issue, but upon probable cause, supported by Oath or affirmation, and particularly describing the place to be searched, and the persons or things to be seized. ";
+    }
+}
+
+function myFifthAmend() {
+    var x = document.getElementById("fifthAmendment");
+    if (x.innerHTML == "No person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury, except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger; nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any criminal case to be a witness against himself, nor be deprived of life, liberty, or property, without due process of law; nor shall private property be taken for public use, without just compensation. ") {
+        x.innerHTML = "Innocent Until Proven Guilty by the judicial system. ";
+    } else {
+        x.innerHTML = "No person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury, except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger; nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any criminal case to be a witness against himself, nor be deprived of life, liberty, or property, without due process of law; nor shall private property be taken for public use, without just compensation. ";
+    }
+}
